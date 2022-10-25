83a31b9190ecd111d20d26d06717c23f1a35657a
Broken_Deer <kxscluabcbook@outlook.com>
2022-10-25 16:59:52 +0800

    清理无用文件

--- public/app/hole.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (file_exists('../../config/hole.json')) {
-    $config = json_decode(file_get_contents('../../config/hole.json'),true);
-    $maximum = count($config['text']);
-    echo $config['text'][mt_rand(0,$maximum-1)];
-}
-else {
-    echo '<p style="color:red;">服务器查询数据失败：关键文件缺失</p>';
-}
